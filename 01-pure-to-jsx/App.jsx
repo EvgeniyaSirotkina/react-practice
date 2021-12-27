4861809e89972cc172e98bea208c7f094aee4fca
Yauheniya Sirotkina <zsirotkina@gmail.com>
2021-12-27 13:03:58 +0300

    add condition with preloader component

--- 01-pure-to-jsx/App.jsx
+++ 01-pure-to-jsx/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { TestFunc } from "./TestFunc";
+import { Preloader } from "./Preloader";
 
-export const App = () => {
-    return (
-      <div>
-        <TestFunc title="Test1" data="test1" />
-        <TestFunc title="Test2" data="test2" />
-        <TestFunc title="Test3" data="test3" />
-      </div>
-    );
+export const App = (props) => {
+    return props.isLoading ? (
+        <Preloader />
+      ) : (
+        <div>
+          <TestFunc title="Test1" data="test1" />
+          <TestFunc title="Test2" data="test2" />
+          <TestFunc title="Test3" data="test3" />
+        </div>
+      );
   };
